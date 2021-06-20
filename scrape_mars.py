0f7aef94190b890d225d9048adc9f5184331d913
OnyNwosu <onuwa.nwosu@gmail.com>
2021-06-20 18:00:45 -0500

    adding to mars.py from notebook

--- scrape_mars.py
+++ scrape_mars.py
@@ -40,12 +40,42 @@ news_p
 
 # JPL Space Images
 
+# Visit URL
+url = 'https://spaceimages-mars.com'
+browser.visit(url)
+
+# Find and click the full image button
+full_image_elem = browser.find_by_tag('button')[1]
+full_image_elem.click()
+
+# Parse the resulting html with soup
+html = browser.html
+img_soup = soup(html, 'html.parser')
+
+# find the relative image url
+img_url_rel = img_soup.find('img', class_='fancybox-image').get('src')
+
+# Use the base url to create an absolute url
+img_url = f'https://spaceimages-mars.com/{img_url_rel}'
 
 
+# Mars Facts
+
+df = pd.read_html('https://galaxyfacts-mars.com')[0]
+df.columns=['Description', 'Mars', 'Earth']
+df.set_index('Description', inplace=True)
+df.to_html()
 
 
+# Hemispeheres
 
+# 1. Use browser to visit the URL 
+url = 'https://marshemispheres.com/'
 
+browser.visit(url)
+
+# 2. Create a list to hold the images and titles.
+hemisphere_image_urls = []
 
 
 
